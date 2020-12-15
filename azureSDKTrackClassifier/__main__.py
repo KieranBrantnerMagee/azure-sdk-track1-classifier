8706af155b8d9bad3ebbbc347eef570cc6d4a915
Kieran Brantner-Magee <kibrantn@microsoft.com>
2020-12-14 16:00:46 -0800

    Add an arg to the command line to allow outputting logs by level when run.

--- azureSDKTrackClassifier/__main__.py
+++ azureSDKTrackClassifier/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 import sys
 
@@ -9,7 +10,9 @@ if __name__ == "__main__":
     parser.add_argument('text', type=str, help='The text to classify as containing T1 content')
     parser.add_argument('--language', default=None, type=str, help='Specify the language to tailor this classification for.  If unspecified, checks for all languages.')
     parser.add_argument('--service', default=None, type=str, help='Specify the service to tailor this classification for.  If unspecified, checks for all services.')
+
     parser.add_argument('--verbose', default=False, action='store_true', help='Enable this flag to produce verbose classification results, including text features as well as prediction.')
+    parser.add_argument('--log-level', default=None, type=str, help='Specify log level (debug, info, warning, exception, error) to output alongside results')
 
     parser.add_argument('--load-from-file', type=str, help='Load a cached model from a local file rather than training from scratch.  Specify the path with this argument.')
     parser.add_argument('--save-to-file', type=str, help='Save the model trained or used in this run to a file so it may be loaded again in the future.  Specify the path with this argument.')
@@ -19,6 +22,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    if args.log_level:
+        logging.basicConfig(level=getattr(logging, args.log_level.upper()))
     if args.load_from_file:
         is_t1_classifier = AzureSDKTrackClassifier.load(args.load_from_file)
     elif args.load_from_blob:
